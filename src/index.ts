3f8ec9ba127d0ae6fb67818113e302e52b5a41f8
misanthropic-codes <misanthropic.codes@outlook.com>
2025-02-12 17:16:38 +0530

    added backend for app

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,177 @@
 import express from "express";
-import mongoose from "mongoose";
+import { random } from "./utils";
 import jwt from "jsonwebtoken";
+import { ContentModel, LinkModel, UserModel } from "./db";
+import { JWT_PASSWORD } from "./config";
+import { userMiddleware } from "./middleware";
+import cors from "cors";
+
+import { Request } from 'express';
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    userId?: string; 
+  }
+}
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.post("api/v1/signup" , (req, res) => {
+app.post("/api/v1/signup", async (req, res) => {
+    // TODO: zod validation , hash the password
+    const username = req.body.username;
+    const password = req.body.password;
 
+    try {
+        await UserModel.create({
+            username: username,
+            password: password
+        }) 
 
-
-
+        res.json({
+            message: "User signed up"
+        })
+    } catch(e) {
+        res.status(411).json({
+            message: "User already exists"
+        })
+    }
 })
 
-app.post("api/v1/signin" , (req, res) => {
+app.post("/api/v1/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+    if (existingUser) {
+        const token = jwt.sign({
+            id: existingUser._id
+        }, JWT_PASSWORD)
+
+        res.json({
+            token
+        })
+    } else {
+        res.status(403).json({
+            message: "Incorrrect credentials"
+        })
+    }
+})
+
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
+    const link = req.body.link;
+    const type = req.body.type;
+    console.log("working till here");
+    await ContentModel.create({
+        link,
+        type,
+        title: req.body.title,
+        userId: req.userId,
+        tags: []
+    })
+
+    res.json({
+        message: "Content added"
+    })
     
 })
 
-
-app.post("api/v1/content" , (req, res) => {
-
-    
+app.get("/api/v1/content", userMiddleware, async (req, res) => {
+    // @ts-ignore
+    const userId = req.userId;
+    const content = await ContentModel.find({
+        userId: userId
+    }).populate("userId", "username")
+    res.json({
+        content
+    })
 })
 
-app.get("api/v1/content" , (req, res) => {
+app.delete("/api/v1/content", userMiddleware, async (req, res) => {
+    const contentId = req.body.contentId;
 
-    
+    await ContentModel.deleteMany({
+        contentId,
+        userId: req.userId
+    })
+
+    res.json({
+        message: "Deleted"
+    })
 })
 
-app.delete("api/v1/content" , (req, res) => {
+app.post("/api/v1/brain/share", userMiddleware, async (req, res) => {
+    const share = req.body.share;
+    if (share) {
+            const existingLink = await LinkModel.findOne({
+                userId: req.userId
+            });
 
-    
+            if (existingLink) {
+                res.json({
+                    hash: existingLink.hash
+                })
+                return;
+            }
+            const hash = random(10);
+            await LinkModel.create({
+                userId: req.userId,
+                hash: hash
+            })
+
+            res.json({
+                hash
+            })
+    } else {
+        await LinkModel.deleteOne({
+            userId: req.userId
+        });
+
+        res.json({
+            message: "Removed link"
+        })
+    }
 })
 
-app.post("api/v1/brain/share" , (req, res) => {
+app.get("/api/v1/brain/:shareLink", async (req, res) => {
+    const hash = req.params.shareLink;
 
-    
+    const link = await LinkModel.findOne({
+        hash
+    });
+
+    if (!link) {
+        res.status(411).json({
+            message: "Sorry incorrect input"
+        })
+        return;
+    }
+    // userId
+    const content = await ContentModel.find({
+        userId: link.userId
+    })
+
+    console.log(link);
+    const user = await UserModel.findOne({
+        _id: link.userId
+    })
+
+    if (!user) {
+        res.status(411).json({
+            message: "user not found, error should ideally not happen"
+        })
+        return;
+    }
+
+    res.json({
+        username: user.username,
+        content: content
+    })
+
 })
 
-app.get("api/v1/brain/:shareLink" , (req, res) => {
-
-    
-})
+app.listen(3000);
---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_PASSWORD } from "./config";
+
+
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    Id?: string; // or the appropriate type for Id
+  }
+}
+
+export const userMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const header = req.headers["authorization"];
+    const decoded = jwt.verify(header as string, JWT_PASSWORD)
+    if (decoded) {
+        if (typeof decoded === "string") {
+            res.status(403).json({
+                message: "You are not logged in"
+            })
+            return;    
+        }
+        req.userId = (decoded as JwtPayload).id;
+        next()
+    } else {
+        res.status(403).json({
+            message: "You are not logged in"
+        })
+    }
+}
